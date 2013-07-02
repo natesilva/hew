54c4edd97dd5ee08b075310a05c11ea7aa0db8d7
Nate Silva <nate@desi.com>
2013-07-02 13:51:48 -0700

    remove unnecessary hierarchy in DNS unit tests

--- tests/dns.js
+++ tests/dns.js
@@ -54,166 +54,146 @@ describe('DNS', function() {
     dns.deleteDomain(dname, done);
   });
 
-  describe('listDomains', function() {
+  it('should get a list of domains', function(done) {
+    dns.listDomains(function(err, result) {
+      should.not.exist(err);
+      should.exist(result);
+      result.should.not.be.empty;
+      result.should.be.an.instanceOf(Array);
+      var names = result.map(function(x) { return x.name; });
+      names.should.include(dname);
+      done();
+    });
+  });
 
-    it('should get a list of domains', function(done) {
-      dns.listDomains(function(err, result) {
+  it('should get one domain', function(done) {
+    dns.getDomain(dname, function(err, result) {
+      should.not.exist(err);
+      should.exist(result);
+      result.name.should.equal(dname);
+      done();
+    });
+  });
+
+  it('should update TTL for a domain', function(done) {
+    var newTtl = 400;
+    dns.updateDomain(dname, 'administrator@company.com', newTtl,
+      function(err, result)
+    {
+      should.not.exist(err);
+      should.exist(result);
+      result.name.should.equal(dname);
+      result.ttl.should.equal(newTtl);
+      done();
+    });
+  });
+
+  it('should get servers for a domain', function(done) {
+    dns.getServers(dname, function(err, result)
+    {
+      should.not.exist(err);
+      should.exist(result);
+      result.should.not.be.empty;
+      result.should.be.an.instanceOf(Array);
+      done();
+    });
+  });
+
+  it('should create a DNS record', function(done) {
+    var type = 'TXT';
+    var data = '"v=spf1 -all"';
+    var ttl = 300;
+    dns.createRecord(dname, dname, 'TXT', data, ttl, function(err, result1) {
+      should.not.exist(err);
+      should.exist(result1);
+      result1.name.should.equal(dname);
+      result1.type.should.equal(type);
+      result1.data.should.equal(data);
+      result1.ttl.should.equal(ttl);
+
+      dns.getRecord(dname, result1.id, function(err, result2) {
         should.not.exist(err);
-        should.exist(result);
-        result.should.not.be.empty;
-        result.should.be.an.instanceOf(Array);
-        var names = result.map(function(x) { return x.name; });
-        names.should.include(dname);
+        result2.id.should.equal(result1.id);
+        result2.name.should.equal(result1.name);
+        result2.type.should.equal(result1.type);
+        result2.data.should.equal(result1.data);
+        result2.ttl.should.equal(result1.ttl);
         done();
       });
     });
   });
 
-  describe('getDomain', function() {
-    it('should get one domain', function(done) {
-      dns.getDomain(dname, function(err, result) {
-        should.not.exist(err);
-        should.exist(result);
-        result.name.should.equal(dname);
-        done();
-      });
+  it('should find multiple A records', function(done) {
+    dns.findRecords(dname, dname, 'A', function(err, records) {
+      should.not.exist(err);
+      should.exist(records);
+      records.should.be.an.instanceOf(Array);
+      records.should.not.be.empty;
+      done();
     });
-
   });
 
-  describe('updateDomain', function() {
-    it('should update TTL for a domain', function(done) {
-      var newTtl = 400;
-      dns.updateDomain(dname, 'administrator@company.com', newTtl,
-        function(err, result)
-      {
+  it('should delete a DNS record', function(done) {
+    dns.findRecords(dname, dname, 'A', function(err, records) {
+      should.not.exist(err);
+      should.exist(records);
+      records.should.not.be.empty;
+      dns.deleteRecord(dname, records[0].id, function(err) {
         should.not.exist(err);
-        should.exist(result);
-        result.name.should.equal(dname);
-        result.ttl.should.equal(newTtl);
         done();
       });
     });
   });
 
-  describe('getServers', function() {
-    it('should get servers for a domain', function(done) {
-      dns.getServers(dname, function(err, result)
-      {
+  it('should update the name of a DNS record', function(done) {
+    dns.findRecords(dname, dname, 'A', function(err, records) {
+      should.not.exist(err);
+      var r = records[0];
+      var newValue = 'users.' + dname;
+      dns.updateRecordName(dname, r.id, newValue, function(err, rec) {
         should.not.exist(err);
-        should.exist(result);
-        result.should.not.be.empty;
-        result.should.be.an.instanceOf(Array);
+        rec.name.should.equal(newValue);
         done();
       });
     });
   });
 
-  describe('createRecord', function() {
-    it('should create a DNS record', function(done) {
-      var type = 'TXT';
-      var data = '"v=spf1 -all"';
-      var ttl = 300;
-      dns.createRecord(dname, dname, 'TXT', data, ttl, function(err, result1) {
+  it('should update the data of a DNS record', function(done) {
+    dns.findRecords(dname, dname, 'MX', function(err, records) {
+      should.not.exist(err);
+      var r = records[0];
+      var newValue = 'incoming.company.com.';
+      dns.updateRecordData(dname, r.id, newValue, function(err, rec) {
         should.not.exist(err);
-        should.exist(result1);
-        result1.name.should.equal(dname);
-        result1.type.should.equal(type);
-        result1.data.should.equal(data);
-        result1.ttl.should.equal(ttl);
-
-        dns.getRecord(dname, result1.id, function(err, result2) {
-          should.not.exist(err);
-          result2.id.should.equal(result1.id);
-          result2.name.should.equal(result1.name);
-          result2.type.should.equal(result1.type);
-          result2.data.should.equal(result1.data);
-          result2.ttl.should.equal(result1.ttl);
-          done();
-        });
-      });
-    });
-  });
-
-  describe('findRecords', function() {
-    it('should find multiple A records', function(done) {
-      dns.findRecords(dname, dname, 'A', function(err, records) {
-        should.not.exist(err);
-        should.exist(records);
-        records.should.be.an.instanceOf(Array);
-        records.should.not.be.empty;
+        rec.data.should.equal(newValue);
         done();
       });
     });
   });
 
-  describe('deleteRecord', function() {
-    it('should delete a DNS record', function(done) {
-      dns.findRecords(dname, dname, 'A', function(err, records) {
+  it('should update the TTL of a DNS record', function(done) {
+    dns.findRecords(dname, dname, 'MX', function(err, records) {
+      should.not.exist(err);
+      var r = records[0];
+      var newValue = 567;
+      dns.updateRecordTtl(dname, r.id, newValue, function(err, rec) {
         should.not.exist(err);
-        should.exist(records);
-        records.should.not.be.empty;
-        dns.deleteRecord(dname, records[0].id, function(err) {
-          should.not.exist(err);
-          done();
-        });
+        rec.ttl.should.equal(newValue);
+        done();
       });
     });
   });
 
-  describe('updateRecord', function() {
-
-    it('should update the name of a DNS record', function(done) {
-      dns.findRecords(dname, dname, 'A', function(err, records) {
+  it('should update the priority of a DNS record', function(done) {
+    dns.findRecords(dname, dname, 'MX', function(err, records) {
+      should.not.exist(err);
+      var r = records[0];
+      var newValue = 100;
+      dns.updateRecordPriority(dname, r.id, newValue, function(err, rec) {
         should.not.exist(err);
-        var r = records[0];
-        var newValue = 'users.' + dname;
-        dns.updateRecordName(dname, r.id, newValue, function(err, rec) {
-          should.not.exist(err);
-          rec.name.should.equal(newValue);
-          done();
-        });
+        rec.priority.should.equal(newValue);
+        done();
       });
     });
-
-    it('should update the data of a DNS record', function(done) {
-      dns.findRecords(dname, dname, 'MX', function(err, records) {
-        should.not.exist(err);
-        var r = records[0];
-        var newValue = 'incoming.company.com.';
-        dns.updateRecordData(dname, r.id, newValue, function(err, rec) {
-          should.not.exist(err);
-          rec.data.should.equal(newValue);
-          done();
-        });
-      });
-    });
-
-    it('should update the TTL of a DNS record', function(done) {
-      dns.findRecords(dname, dname, 'MX', function(err, records) {
-        should.not.exist(err);
-        var r = records[0];
-        var newValue = 567;
-        dns.updateRecordTtl(dname, r.id, newValue, function(err, rec) {
-          should.not.exist(err);
-          rec.ttl.should.equal(newValue);
-          done();
-        });
-      });
-    });
-
-    it('should update the priority of a DNS record', function(done) {
-      dns.findRecords(dname, dname, 'MX', function(err, records) {
-        should.not.exist(err);
-        var r = records[0];
-        var newValue = 100;
-        dns.updateRecordPriority(dname, r.id, newValue, function(err, rec) {
-          should.not.exist(err);
-          rec.priority.should.equal(newValue);
-          done();
-        });
-      });
-    });
-
   });
 });
